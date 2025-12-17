dc98027cfb23f299d84e0b4780a6088edb6103db
Manoel Lima <manoel.lima@manaus.am.gov.br>
2025-12-17 14:17:23 -0400

    feat: add contato component with template, styles, and tests; update app component to include it

--- indexa/src/app/app.component.ts
+++ indexa/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from "./componentes/container/container.component";
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { SeparadorComponent } from './componentes/separador/separador.component';
+import { ContatoComponent } from './componentes/contato/contato.component';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +13,7 @@ import { SeparadorComponent } from './componentes/separador/separador.component'
     CommonModule,
     RouterOutlet,
     ContainerComponent,
+    ContatoComponent,
     CabecalhoComponent,
     SeparadorComponent
   ],
